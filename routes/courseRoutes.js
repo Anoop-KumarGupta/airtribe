fc83a6b2413c6c7fdb79ebb7567ef8e2947bb840
Anoop Kumar Gupta <118035198+Anoop-KumarGupta@users.noreply.github.com>
2024-03-11 00:41:22 +0530

    Update courseRoutes.js

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -4,7 +4,8 @@ const router = express.Router();
 const authController=require('./../controllers/authController');
 
 
- 
+ // used multiple middlewares for protecting routes and also for the restrictions
+// basic functions of API
 router
   .route("/")
   .get(authController.protect,courseController.getAllCourses)
@@ -16,7 +17,7 @@ router
   .delete(authController.protect,authController.restricTo('instructor'),courseController.deleteCourse);
 
 
-// API functionalities
+// extra API functionalities
 router.route("/:id/registration").post(authController.protect,authController.restricTo('student'),courseController.registerForCourse);
 router.route("/:id/getAllLeads").get(authController.protect,authController.restricTo('instructor'),courseController.getAllLeads);
 router.route("/:id/comment").post(authController.protect,authController.restricTo('instructor'),courseController.addComment);
