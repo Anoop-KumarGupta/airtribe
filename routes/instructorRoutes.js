95cb66ba524670e495b108fd5e4cc0553e71a9fb
Anoop Kumar Gupta <118035198+Anoop-KumarGupta@users.noreply.github.com>
2024-03-11 00:42:37 +0530

    Update instructorRoutes.js

--- routes/instructorRoutes.js
+++ routes/instructorRoutes.js
@@ -3,9 +3,13 @@ const router=express.Router();
 const instructorController=require('./../controllers/instructorController');
 const authController=require('./../controllers/authController');
 
+
+// signup and login functionalities
 router.post('/signup',authController.signup);
 router.post('/login',authController.login);
 
+
+// other functionalities like get list of instructors, updating instructor details, etc.
 router.route('/').get(instructorController.getAllInstructors).post(instructorController.updateInstructor);
 router.route('/:instructorID').get(instructorController.getInstructor);
 module.exports=router; 
